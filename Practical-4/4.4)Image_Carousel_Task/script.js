8e9b57b930ecc3828f4981958c7c5ddc01476720
Sujan Butani <sujan.devotee@gmail.com>
2024-07-12 17:43:31 +0530

    Practical Task-4.4 image carousel Done.

---
+++ Practical-4/4.4)Image_Carousel_Task/script.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+      showSlides();
+
+      function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        if (slideIndex >= slides.length) {
+          slideIndex = 0;
+        }
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        slides[slideIndex].style.display = "block";
+        slideIndex++;
+      }
+      setInterval(showSlides, 5000);
+
+      // Next/previous controls
+      function plusSlides(n) {
+        showSlides(slideIndex += n);
+      }
